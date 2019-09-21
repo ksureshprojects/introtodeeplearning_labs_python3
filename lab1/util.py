092d00588c74f71347c8a3ed79cbc150bf4fa893
ksureshprojects <53956900+ksureshprojects@users.noreply.github.com>
2019-09-21 17:13:32 +0100

    Update util.py

--- lab1/util.py
+++ lab1/util.py
@@ -7,7 +7,7 @@ def extract_song_snippet(generated_text):
     pattern = '\n\n(.*?)\n\n'
     search_results = re.findall(pattern, generated_text, overlapped=True, flags=re.DOTALL)
     songs = [song for song in search_results]
-    print "Found {} possible songs in generated texts".format(len(songs))
+    print("Found {} possible songs in generated texts".format(len(songs)))
     return songs
 
 def save_song_to_abc(song, filename="tmp"):
@@ -28,11 +28,11 @@ def play_wav(wav_file):
 def play_generated_song(generated_text):
     songs = extract_song_snippet(generated_text)
     if len(songs) == 0:
-        print "No valid songs found in generated text. Try training the model longer or increasing the amount of generated music to ensure complete songs are generated!"
+        print("No valid songs found in generated text. Try training the model longer or increasing the amount of generated music to ensure complete songs are generated!")
 
     for song in songs:
         basename = save_song_to_abc(song)
         ret = abc2wav(basename+'.abc')
         if ret == 0: #did not suceed
             return play_wav(basename+'.wav')
-    print "None of the songs were valid, try training longer to improve syntax."
+    print("None of the songs were valid, try training longer to improve syntax.")
